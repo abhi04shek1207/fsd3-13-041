f43cc510db3f64a04dcd4f159235266fe4eca766
Abhishek <abhishek.4bhardwaj0@gmail.com>
2026-07-22 11:23:15 +0530

    arrow fxn

--- LAB1/arrow.js
+++ LAB1/arrow.js
@@ -1,5 +1,5 @@
 // write a fxn  number b/w 0 to 9  and return in words using arrow fxn
-const numberToWords = (num) => {
+/*const numberToWords = (num) => {
   switch (num) {
     case 0: return 'Zero';
     case 1: return 'One';
@@ -17,7 +17,14 @@ const numberToWords = (num) => {
 console.log(numberToWords(0)); 
 console.log(numberToWords(5)); 
 console.log(numberToWords(9)); 
-console.log(numberToWords(12)); 
+console.log(numberToWords(12)); */
+
+
+//create another fxn that takes a number show in words with the hlp of towards fxn
+const numberToWords = (num) => {
+  const words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  return words[num] || 'Invalid';
+};
 
 
 
