1fd9c3dae785ce3adc8f38b21fe6c79d1e23173e
Abhishek <abhishek.4bhardwaj0@gmail.com>
2026-07-28 14:40:26 +0530

    eventloop

--- LAB1/arrow.js
+++ LAB1/arrow.js
@@ -1,5 +1,5 @@
 // write a fxn  number b/w 0 to 9  and return in words using arrow fxn
-/*const numberToWords = (num) => {
+const toWords = (num) => {
   switch (num) {
     case 0: return 'Zero';
     case 1: return 'One';
@@ -14,17 +14,31 @@
     default: return 'Invalid';
   }
 };
-console.log(numberToWords(0)); 
-console.log(numberToWords(5)); 
-console.log(numberToWords(9)); 
-console.log(numberToWords(12)); */
+console.log(toWords(0)); 
+console.log(toWords(5)); 
+console.log(toWords(9)); 
+console.log(toWords(12)); 
 
 
 //create another fxn that takes a number show in words with the hlp of towards fxn
-const numberToWords = (num) => {
-  const words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
-  return words[num] || 'Invalid';
-};
+
+/*const rollnum="2503201000041";
+const digit =String(rollnum).split("");
+console.log(digit);*/
+//USING FOR EACH LOOP:-
+const rollNum = "2503201000041";
+const digits = String(rollNum).split("");
+console.log(digits);
+
+let inWords = "";
+
+digits.forEach((d) => {
+    inWords += " " + toWords(Number(d));
+});
+
+console.log(inWords);
+
+
 
 
 
