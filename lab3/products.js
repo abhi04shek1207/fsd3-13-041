6110224eb173ee59e125b623c7c7b2404af1ca57
Abhishek <abhishek.4bhardwaj0@gmail.com>
2026-09-22 14:17:42 +0530

    get all product commit

---
+++ lab3/products.js
@@ -0,0 +1,9 @@
+ const products=[
+{id:1,name:'marker', price:15,qty:100 },
+{id:2,name:'duster',price:24,qty:560}
+
+]
+let nextID = 3;
+ export const getAllProducts = () => {
+    return products;
+ }
